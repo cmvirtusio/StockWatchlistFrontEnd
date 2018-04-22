2507fb1ea0b71d3af17d68d88eff9ce88ea9f17c
Charles Virtusio <virtusio.charlesmartin@gmail.com>
2018-04-22 18:03:54 -0400

    First Commit, skeleton components, router setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { WatchlistsComponent } from './components/watchlists/watchlists.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  //home
+  {path:'', component: HomeComponent},
+  //login
+  {path:'login', component: LoginComponent},
+  //dashboard
+  {path:'dashboard', component: DashboardComponent},
+  //watchlists (This should be protected)
+  {path:'watchlists', component: WatchlistsComponent},
+  //register
+  {path:'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { WatchlistsComponent } from './components/watchlists/watchlists.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent,
+    WatchlistsComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
