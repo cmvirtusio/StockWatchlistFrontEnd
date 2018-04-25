7760e620174c25adac02f9c23d021099f3d51206
Charles Virtusio <virtusio.charlesmartin@gmail.com>
2018-04-25 01:57:30 -0400

    Added Regex test for 'production'

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -15,52 +15,62 @@ export class RegisterComponent implements OnInit {
   //password
   //passwordConfirmation
   //No need to create a model, coz it is only used by this Component
-  username : String;
-  password : String;
-  passwordConfirmation : String;
+  username;
+  password;
+  passwordConfirmation;
 
   //dependency injection
   constructor(
-    private formvalidationservice : FormvalidationService,
-    private authservice : AuthService,
-    private flash : FlashMessagesService,
-    private router : Router
+    private formvalidationservice: FormvalidationService,
+    private authservice: AuthService,
+    private flash: FlashMessagesService,
+    private router: Router
   ) { }
 
   ngOnInit() {
   }
 
-  onRegisterSubmit(){
+  onRegisterSubmit() {
     const registerObject = {
-      username : this.username,
-      password : this.password,
-      passwordConfirmation : this.passwordConfirmation
+      username: this.username,
+      password: this.password,
+      passwordConfirmation: this.passwordConfirmation
     };
     // Required Fields
-    if(!this.formvalidationservice.validateRegister(registerObject)) {
-      this.flash.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+    if (!this.formvalidationservice.validateRegister(registerObject)) {
+      this.flash.show('Please fill in all fields', { cssClass: 'alert-danger', timeout: 3000 });
       return false;
-    } else {
-      //run comparepasswords
-      if(!this.formvalidationservice.comparePassword(registerObject)){
-        this.flash.show('Passwords do not match', {cssClass: 'alert-danger', timeout: 3000});
-      }
     }
-
+    // Passwords must match
+    if (!this.formvalidationservice.comparePassword(registerObject)) {
+      this.flash.show('Passwords do not match', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+    //Run username password through regex
+    const regex = /^[a-zA-Z0-9.\-_$@*!]{8,32}$/;
+    console.log("hello");
+    if(!regex.test(registerObject.username)){
+      this.flash.show('Username not valid must be 8-32 in length and some special characters', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+    if(!regex.test(registerObject.password)){
+      this.flash.show('Password not valid must be 8-32 in length and some special characters', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
     //Register user
     this.authservice.registerUser(registerObject).subscribe(
       suc => {
         const message = suc['body'];
-        if(suc['body'] == 'User Created'){
-          this.flash.show(message, {cssClass: 'alert-success', timeout: 3000});
+        if (suc['body'] == 'User Created') {
+          this.flash.show(message, { cssClass: 'alert-success', timeout: 3000 });
           this.router.navigate(['/login']);
         } else {
-          this.flash.show(message, {cssClass: 'alert-danger', timeout: 3000});
+          this.flash.show(message, { cssClass: 'alert-danger', timeout: 3000 });
           this.router.navigate(['/register']);
         }
       },
       err => {
-        this.flash.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+        this.flash.show('Something went wrong', { cssClass: 'alert-danger', timeout: 3000 });
         this.router.navigate(['/register']);
       }
     );
